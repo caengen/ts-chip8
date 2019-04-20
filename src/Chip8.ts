34843162c1669d2be1f728831f3d746a947e44b4
Christian Engen <chris.a.engen@gmail.com>
2019-04-20 11:10:40 +0200

    Add scaffold comments

--- src/Chip8.ts
+++ src/Chip8.ts
@@ -21,12 +21,51 @@ export default class Chip8 {
   // key state
   public key: Int8Array = new Int8Array(16);
 
-  constructor() {
+  /**
+   * The system does not draw each cycle. This flag is set when the system
+   * should draw. These opcodes sets the flag:
+   * 0x00E0 - Clear screen
+   * 0xDXYN - Draw a sprite on the screen
+   */
+  public drawFlag: boolean;
+
+  public constructor() {
     this.opcode = 0x0;
     this.I = 0x0;
     this.pc = 0x0;
     this.delayTimer = 0x0;
     this.soundTimer = 0x0;
     this.sp = 0x0;
+    this.drawFlag = false;
+  }
+
+  /**
+   * Clears the memory, registers and screen
+   */
+  public initialize() {
+    // Initialize registers and memory once
+  }
+
+  /**
+   * Copies program into memory
+   */
+  public loadGame() {
+
+  }
+
+  public emulateCycle() {
+    // Fetch Opcode
+    // opcode are two bytes so must be merged OR
+    // opcode = memory[pc] << 8 | memory[pc + 1]
+    // Decode Opcode
+    // [operation, operand] = decode(opcode)
+    // Execute Opcode
+    // execute(operation, operand)
+
+    // Update timers
+  }
+
+  public setKeys() {
+
   }
 }
