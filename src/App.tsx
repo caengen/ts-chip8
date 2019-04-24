b0dfa077be5c21088e4fd36251b926131217b762
Christian Engen <chris.a.engen@gmail.com>
2019-04-24 08:43:54 +0200

    Eject and import binary correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import logo from "./logo.svg";
 import './App.css';
 import Chip8 from './Chip8';
-const pong = require("./roms/Pong.ch8");
+import pong from "./roms/Pong.ch8";
 
-class App extends Component {
-  render() {
-    const chip8 = new Chip8();
-    chip8.loadGame(pong);
-    chip8.dissassemble();
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  const chip8 = new Chip8();
+  console.log(pong);
+  chip8.loadGame(pong);
+  chip8.dissassemble();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
