a2e48829e519b86c643f3d9c45224d9017176630
Christian Engen <chris.a.engen@gmail.com>
2019-04-26 22:00:15 +0200

    Fix instruction comments

--- src/Chip8.ts
+++ src/Chip8.ts
@@ -137,84 +137,103 @@ export default class Chip8 {
       case 8:
         switch (opc & 0x000F) {
           case 0:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Sets VX to the value of VY.`)
             break;
           case 1:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Sets VX to VX or VY. (Bitwise OR operation)`)
             break;
           case 2:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Sets VX to VX and VY. (Bitwise AND operation)`)
             break;
           case 3:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Sets VX to VX xor VY.`)
             break;
           case 4:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.`)
             break;
           case 5:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.`)
             break;
           case 6:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Stores the least significant bit of VX in VF and then shifts VX to the right by 1.`)
             break;
           case 7:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.`)
             break;
           case 0xE:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Stores the most significant bit of VX in VF and then shifts VX to the left by 1.`)
             break;
         }
         break;
+      case 9:
+        console.log(`${opc.toString(16)} Skips the next instruction if VX doesn't equal VY. (Usually the next instruction is a jump to skip a code block)`)
+        break;
       case 0xA:
-        console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+        console.log(`${opc.toString(16)} Sets I to the address NNN.`)
         break;
       case 0xB:
-        console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+        console.log(`${opc.toString(16)} Jumps to the address NNN plus V0`)
         break;
       case 0xC:
-        console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+        console.log(`${opc.toString(16)} Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.`)
         break;
       case 0xD:
-        console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+      /**
+       * Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and
+       * a height of N pixels. Each row of 8 pixels is read as bit-coded 
+       * starting from memory location I; I value doesn’t change after the 
+       * execution of this instruction. As described above, VF is set to 1 if 
+       * any screen pixels are flipped from set to unset when the sprite is 
+       * drawn, and to 0 if that doesn’t happen
+       */
+        console.log(`${opc.toString(16)} Draws a sprite at coordinate (VX, VY)`)
         break;
       case 0xE:
         switch (opc & 0x000F) {
           case 0xE:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Skips the next instruction if the key stored in VX is pressed. (Usually the next instruction is a jump to skip a code block).`)
             break;
           case 1:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Skips the next instruction if the key stored in VX isn't pressed. (Usually the next instruction is a jump to skip a code block).`)
             break;
         }
         break;
       case 0xF:
         switch (opc & 0x00FF) {
           case 7:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Sets VX to the value of the delay timer.`)
             break;
           case 0xA:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} A key press is awaited, and then stored in VX. (Blocking Operation. All instruction halted until next key event)`)
             break;
           case 15:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Sets the delay timer to VX.`)
             break;
           case 18:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Sets the sound timer to VX.`)
             break;
           case 0x1E:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Adds VX to I.`)
             break;
           case 29:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font.`)
             break;
           case 33:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+          /**
+           * Stores the binary-coded decimal representation of VX, with the 
+           * most significant of three digits at the address in I, the 
+           * middle digit at I plus 1, and the least significant digit at 
+           * I plus 2. (In other words, take the decimal representation of 
+           * VX, place the hundreds digit in memory at location in I, the 
+           * tens digit at location I+1, and the ones digit at location I+2.)
+           */
+            console.log(`${opc.toString(16)} Store binary-coded decimal representation of VX`)
             break;
           case 55:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Stores V0 to VX (including VX) in memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.`)
             break;
           case 65:
-            console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
+            console.log(`${opc.toString(16)} Fills V0 to VX (including VX) with values from memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.`)
             break;
         }
         break;
