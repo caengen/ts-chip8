64fedee25aab92dab568bc12b5435bbd8f80694d
Christian Engen <chris.a.engen@gmail.com>
2019-04-26 22:07:32 +0200

    Fix binary cases

--- src/Chip8.ts
+++ src/Chip8.ts
@@ -101,83 +101,83 @@ export default class Chip8 {
    */
   private execute(opc: number) {
     switch (opc & 0xF000) {
-      case 0:
-        switch (opc & 0x00FF) {
-          case 0x00E0:
+      case 0x0000:
+        switch (opc & 0x000F) {
+          case 0x0000:
             console.log("0x00E0 Clear screen")
             this.gfx = new Uint8Array(64 * 32);
             break;
-          case 0x00EE:
+          case 0x000E:
             console.log("0x00EE Return from subroutine")
             break;
           default:
             console.log(`${opc.toString(16)} call RCA 1802 program at NNN`)
         }
-      case 1:
+      case 0x1000:
         console.log(`${opc.toString(16)} jump to address NNN`)
         break;
-      case 2:
+      case 0x2000:
         console.log(`${opc.toString(16)} call subroutine at address NNN`)
         break;
-      case 3:
+      case 0x3000:
         console.log(`${opc.toString(16)} Skips the next instruction if VX equals NN.`)
         break;
-      case 4:
+      case 0x4000:
         console.log(`${opc.toString(16)} Skips the next instruction if VX doesn't equal NN. `)
         break;
-      case 5:
+      case 0x5000:
         console.log(`${opc.toString(16)} Skips the next instruction if VX equals VY. `)
         break;
-      case 6:
+      case 0x6000:
         console.log(`${opc.toString(16)} Sets VX to NN.`)
         break;
-      case 7:
+      case 0x7000:
         console.log(`${opc.toString(16)} Adds NN to VX. (Carry flag is not changed)`)
         break;
-      case 8:
+      case 0x8000:
         switch (opc & 0x000F) {
-          case 0:
+          case 0x0000:
             console.log(`${opc.toString(16)} Sets VX to the value of VY.`)
             break;
-          case 1:
+          case 0x0001:
             console.log(`${opc.toString(16)} Sets VX to VX or VY. (Bitwise OR operation)`)
             break;
-          case 2:
+          case 0x0002:
             console.log(`${opc.toString(16)} Sets VX to VX and VY. (Bitwise AND operation)`)
             break;
-          case 3:
+          case 0x0003:
             console.log(`${opc.toString(16)} Sets VX to VX xor VY.`)
             break;
-          case 4:
+          case 0x0004:
             console.log(`${opc.toString(16)} Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.`)
             break;
-          case 5:
+          case 0x0005:
             console.log(`${opc.toString(16)} VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.`)
             break;
-          case 6:
+          case 0x0006:
             console.log(`${opc.toString(16)} Stores the least significant bit of VX in VF and then shifts VX to the right by 1.`)
             break;
-          case 7:
+          case 0x0007:
             console.log(`${opc.toString(16)} Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.`)
             break;
-          case 0xE:
+          case 0x000E:
             console.log(`${opc.toString(16)} Stores the most significant bit of VX in VF and then shifts VX to the left by 1.`)
             break;
         }
         break;
-      case 9:
+      case 0x9000:
         console.log(`${opc.toString(16)} Skips the next instruction if VX doesn't equal VY. (Usually the next instruction is a jump to skip a code block)`)
         break;
-      case 0xA:
+      case 0xA000:
         console.log(`${opc.toString(16)} Sets I to the address NNN.`)
         break;
-      case 0xB:
+      case 0xB000:
         console.log(`${opc.toString(16)} Jumps to the address NNN plus V0`)
         break;
-      case 0xC:
+      case 0xC000:
         console.log(`${opc.toString(16)} Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.`)
         break;
-      case 0xD:
+      case 0xD000:
       /**
        * Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and
        * a height of N pixels. Each row of 8 pixels is read as bit-coded 
@@ -188,37 +188,37 @@ export default class Chip8 {
        */
         console.log(`${opc.toString(16)} Draws a sprite at coordinate (VX, VY)`)
         break;
-      case 0xE:
+      case 0xE000:
         switch (opc & 0x000F) {
-          case 0xE:
+          case 0x000E:
             console.log(`${opc.toString(16)} Skips the next instruction if the key stored in VX is pressed. (Usually the next instruction is a jump to skip a code block).`)
             break;
-          case 1:
+          case 0x0001:
             console.log(`${opc.toString(16)} Skips the next instruction if the key stored in VX isn't pressed. (Usually the next instruction is a jump to skip a code block).`)
             break;
         }
         break;
-      case 0xF:
+      case 0xF000:
         switch (opc & 0x00FF) {
-          case 7:
+          case 0x0007:
             console.log(`${opc.toString(16)} Sets VX to the value of the delay timer.`)
             break;
-          case 0xA:
+          case 0x000A:
             console.log(`${opc.toString(16)} A key press is awaited, and then stored in VX. (Blocking Operation. All instruction halted until next key event)`)
             break;
-          case 15:
+          case 0x0015:
             console.log(`${opc.toString(16)} Sets the delay timer to VX.`)
             break;
-          case 18:
+          case 0x0018:
             console.log(`${opc.toString(16)} Sets the sound timer to VX.`)
             break;
-          case 0x1E:
+          case 0x001E:
             console.log(`${opc.toString(16)} Adds VX to I.`)
             break;
-          case 29:
+          case 0x0029:
             console.log(`${opc.toString(16)} Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font.`)
             break;
-          case 33:
+          case 0x0033:
           /**
            * Stores the binary-coded decimal representation of VX, with the 
            * most significant of three digits at the address in I, the 
@@ -229,10 +229,10 @@ export default class Chip8 {
            */
             console.log(`${opc.toString(16)} Store binary-coded decimal representation of VX`)
             break;
-          case 55:
+          case 0x0055:
             console.log(`${opc.toString(16)} Stores V0 to VX (including VX) in memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.`)
             break;
-          case 65:
+          case 0x0065:
             console.log(`${opc.toString(16)} Fills V0 to VX (including VX) with values from memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.`)
             break;
         }
